1714ee2adad7a4dd6f9d123e6e5155c6c1be4c86
Jason Parker <jason@jasparke.net>
2017-10-02 02:44:33 -0700

    process list done, fixed helpers. bglist complete

--- pman.c
+++ pman.c
@@ -10,30 +10,58 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <malloc.h>
 #include <sys/types.h>
 #include <readline/readline.h>
+#include <readline/history.h>
 
-#define MAX_INPUT_LEN 80
 
+#define MAX_LEN 120
+#define RUNNING 1
+#define STOPPED 0
+
+typedef struct node_t {
+	pid_t pid;
+	int status;
+	char* proc;
+	struct node_t* next;
+	struct node_t* prev;
+} node_t;
+
+node_t* listHead = NULL;
+node_t* listTail = NULL;
 
 void bg(char* process) {
 
 }
 
-void bgkill(int pid) {
+void bgkill(pid_t pid) {
 
 }
 
-void bgstart(int pid) {
+void bgstart(pid_t pid) {
 
 }
 
-void bgstop(int pid) {
+void bgstop(pid_t pid) {
 
 }
 
+void pstat(pid_t pid) {
+
+}
+
+//prints out a list of all jobs with their pid, and the total count of jobs.
 void bglist() {
+	int count = 0;
+	node_t* curr = listHead;
 
+	while (curr != NULL) {
+		printf("%d:\t %s %s\n",curr->pid, curr->proc, (curr->status == STOPPED)? "(stopped)" : "(running)");
+		count++;
+		curr = curr->next;
+	}
+	printf("Total background jobs: \t %d", count);
 }
 
 void updateProcess() {
@@ -41,6 +69,8 @@ void updateProcess() {
 }
 
 int readInput(char** input) {
+
+
 	printf("readInput() called.");
 	return true;
 }
@@ -49,19 +79,48 @@ void updateFromInput(input) {
 	printf("updateFromInput() called.");
 }
 
+
+/* ### LIST FUNCTIONS ### */
+
+// Append a process to the process list
+void appendNode(pid_t pid, char* proc) {
+	node_t* new_proc = (node_t*)malloc(sizeof(node_t));
+	new_proc->pid = pid;
+	new_proc->proc = proc;
+	new_proc->status = RUNNING;
+	new_proc->next = NULL;
+	new_proc->prev = listTail;
+
+	listTail = new_proc;
+	if (listHead == NULL) { listHead = new_proc; }
+}
+
+void removeNode(pid_t pid) {
+	node_t* curr = listHead;
+
+	while (curr != NULL) {
+		if (curr->pid == pid) {
+			if (curr == listHead) listHead = curr->next;
+			if (curr == listTail) listTail = curr->prev;
+			curr->next->prev = curr->prev;
+			curr->prev->next = curr->next;
+			return;
+		}
+		curr = curr->next;
+	}
+	printf("ERR: Process %d does not exist.", pid);
+}
+
 /**
  * read the input from user and make sure its valid
  * @param input
  * @return true if no issue, false otherwise
  */
 
-/**
- * build the prompt for user input
- * @return n/a
- */
+// build the prompt for user input and runs the main program loop.
 int main() {
 	 while(true) {
-		 char* input[MAX_INPUT_LEN];
+		 char* input[MAX_LEN];
 
 		 updateProcess();
 		 if (readInput(input) {
